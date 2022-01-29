f646d9cab91f52629e40fc53a49d4e942cfa216a
Nikolai <zhniolay@bc.ru>
2022-01-29 20:08:43 +0300

    first commit

---
+++ index.c
@@ -0,0 +1,7 @@
+// пробный файл
+#include <stdoi.h>
+
+int main(void){ 
+  printf("Hallo Git\n");
+ return 0;
+}
---
+++ script.c
@@ -0,0 +1,11 @@
+/**/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+  char arr[7];
+    scanf("%s", arr);
+  puts("\n");
+  printf("%s\n", arr);
+ return 0;
+}
