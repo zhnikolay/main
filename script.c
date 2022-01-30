605573db0074b60beafd6f917fc5681ee3e3d174
Nikolai <zhniolay@bc.ru>
2022-01-30 11:44:41 +0300

    change log

--- script.c
+++ script.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 int main(void){
-  char arr[7];
+  char arr[10];
+    printf("Enter a word\n");
     scanf("%s", arr);
-  puts("\n");
-  printf("%s", arr);
-  puts("\n");
+    puts("\n");
+    printf("%s", arr);
+    puts("\n");
  return 0;
 }
